1c39c6af6f8e50d0729b14cb6ff3fdd1ebd0abd6
黄少 <12116505+SocEternity@user.noreply.gitee.com>
2024-09-13 21:29:20 +0800

    21:29

--- lua/plugins/venv_selector.lua
+++ lua/plugins/venv_selector.lua
@@ -2,7 +2,7 @@ return {
   "linux-cultist/venv-selector.nvim",
   dependencies = { "neovim/nvim-lspconfig", "nvim-telescope/telescope.nvim", "mfussenegger/nvim-dap-python" },
   opts = {
-    -- stay_on_this_version = true,
+    stay_on_this_version = true,
     -- Your options go here
     -- name = "venv",
     -- auto_refresh = false
